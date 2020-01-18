bab50f3a6034fd06cec1e49988511be94ff5df0c
unknown <olga.kuksa@gmail.com>
2020-01-18 20:53:21 +0300

    feat: add SourceSelector as separate component

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -1,34 +1,17 @@
 import React from "react";
+import SourceSelector from "./SourceSelector";
 
 class PageView extends React.Component {
   constructor() {
     super();
-    this.state = { gridId: "fast", dataSource: "jest" };
+    this.state = { selectedComponentId: "fast"};
   }
-
-  onDataSourceChange = e => {
-    this.setState(
-      { dataSource: e.target.value }
-    );
-    //TODO - dispatch data fetch action
-  };
 
   render() {
     return (
       <div>
-        <div>
-          Issues from:
-          <select
-            name="data-source"
-            onChange={this.onDataSourceChange}
-            value={this.state.dataSource}
-          >
-            <option value="local">Local (react repo)</option>
-            <option value="enzyme">Enzyme repo</option>
-            <option value="jest">Jest repo</option>
-          </select>
-        </div>
-        <div>components menu - fast and slow - stub</div>
+        <SourceSelector/>
+        <div id="component-selector"></div>
         <div>filter stub</div>
         <div>grid stub</div>
       </div>
---
+++ src/components/SourceSelector.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SourceSelector extends React.Component {
+  constructor(props) {
+    super();
+    this.state = { dataSource: "local" };
+  }
+
+  onDataSourceChange = e => {
+    this.setState({ dataSource: e.target.value });
+    //TODO - dispatch data fetch action
+  };
+
+  render() {
+    return (
+      <div id="source-selector">
+        Issues from:
+        <select
+          name="data-source"
+          onChange={this.onDataSourceChange}
+          value={this.state.dataSource}
+        >
+          <option value="local">Local (react repo)</option>
+          <option value="enzyme">Enzyme repo</option>
+          <option value="jest">Jest repo</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default SourceSelector;
