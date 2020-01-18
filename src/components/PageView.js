35ebe241c0f37ff871068ec21b070cea2734cacf
unknown <olga.kuksa@gmail.com>
2020-01-18 20:42:09 +0300

    feat: add data source select to PageView

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -3,13 +3,31 @@ import React from "react";
 class PageView extends React.Component {
   constructor() {
     super();
-    this.state = { gridId: "fast", dataSource: "local" };
+    this.state = { gridId: "fast", dataSource: "jest" };
   }
+
+  onDataSourceChange = e => {
+    this.setState(
+      { dataSource: e.target.value }
+    );
+    //TODO - dispatch data fetch action
+  };
 
   render() {
     return (
       <div>
-        <div>data source stub</div>
+        <div>
+          Issues from:
+          <select
+            name="data-source"
+            onChange={this.onDataSourceChange}
+            value={this.state.dataSource}
+          >
+            <option value="local">Local (react repo)</option>
+            <option value="enzyme">Enzyme repo</option>
+            <option value="jest">Jest repo</option>
+          </select>
+        </div>
         <div>components menu - fast and slow - stub</div>
         <div>filter stub</div>
         <div>grid stub</div>
