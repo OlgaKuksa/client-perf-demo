6fbad223ad82611b7ec471d2aa60a0368a26eb31
unknown <olga.kuksa@gmail.com>
2020-01-19 16:39:29 +0300

    feat: add simple styles for components menu

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -2,17 +2,48 @@ import React from "react";
 import SourceSelector from "./SourceSelector";
 import Filter from "./Filter";
 
+import "./PageView.css";
+
 class PageView extends React.Component {
   constructor() {
     super();
     this.state = { selectedComponentId: "fast" };
   }
 
+  makeComponentActive = e => {
+    this.setState({ selectedComponentId: e.target.id });
+  };
+
   render() {
     return (
       <div>
         <SourceSelector />
-        <div id="component-selector"></div>
+        <div>
+          <span
+            onClick={this.makeComponentActive}
+            id="slow"
+            className={
+              "component-box " +
+              (this.state.selectedComponentId === "slow"
+                ? "active"
+                : "inactive")
+            }
+          >
+            Slow component
+          </span>
+          <span
+            onClick={this.makeComponentActive}
+            id="fast"
+            className={
+              "component-box " +
+              (this.state.selectedComponentId === "fast"
+                ? "active"
+                : "inactive")
+            }
+          >
+            Fast Component
+          </span>
+        </div>
         <Filter />
         <div>grid stub</div>
       </div>
