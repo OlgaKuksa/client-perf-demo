285f87abab048f02968ea853625805f70419c19f
unknown <olga.kuksa@gmail.com>
2020-01-26 21:02:56 +0300

    build: add files for gh pages

---
+++ docs/precache-manifest.5a6a5c26b4d71061c6807421b3d819e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874684a1e9d8f0df289122dccc9a542d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b9364b7970809da71d",
+    "url": "/static/css/main.008f2ab5.chunk.css"
+  },
+  {
+    "revision": "5d1eddc29923eec089f9",
+    "url": "/static/js/2.dcb49aad.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.dcb49aad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90b9364b7970809da71d",
+    "url": "/static/js/main.e0d0bcdf.chunk.js"
+  },
+  {
+    "revision": "79b273636d6d63efe374",
+    "url": "/static/js/runtime-main.c670abfe.js"
+  }
+]);
